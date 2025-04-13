31951200edb927a2d95b7ed4ddc1cfe21b65daa1
Rong Chang <rong@us.ibm.com>
2025-04-12 23:03:57 -0400

    test local .tekton/docker-push.yaml

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! new update_deployment.yaml"
+    return "Hello World! use customized .tekton/docker-push.yaml"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
