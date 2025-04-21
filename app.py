2b592068194cbd2356c0ec4fae0a091f93534872
Rong Chang <rong@us.ibm.com>
2025-04-20 22:16:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World! ci/cd test"
+    return "Hello World! pr-success-merge test"
 
 if __name__ == '__main__':
     port = os.environ.get('FLASK_PORT') or 8080
